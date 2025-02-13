a42fcf85e34cbcd26aa9bd99a3b751826b6f391a
BithreenGirlen <152838289+BithreenGirlen@users.noreply.github.com>
2025-02-13 16:35:28 +0900

    Take DPI into consideration.

--- TaimaninPlayer/media_setting_dialogue.cpp
+++ TaimaninPlayer/media_setting_dialogue.cpp
@@ -47,8 +47,12 @@ bool CMediaSettingDialogue::Open(HINSTANCE hInstance, HWND hWnd, void* pMediaPla
         m_hParentWnd = hWnd;
         m_pMediaPlayer = pMediaPlayer;
 
+        UINT uiDpi = ::GetDpiForSystem();
+        int iWindowWidth = ::MulDiv(100, uiDpi, USER_DEFAULT_SCREEN_DPI);
+        int iWindowHeight = ::MulDiv(200, uiDpi, USER_DEFAULT_SCREEN_DPI);
+
         m_hWnd = ::CreateWindowW(m_swzClassName, pwzWindowName, WS_OVERLAPPEDWINDOW & ~ WS_MINIMIZEBOX & ~ WS_MAXIMIZEBOX & ~WS_THICKFRAME,
-            CW_USEDEFAULT, CW_USEDEFAULT, 100, 200, hWnd, nullptr, hInstance, this);
+            CW_USEDEFAULT, CW_USEDEFAULT, iWindowWidth, iWindowHeight, hWnd, nullptr, hInstance, this);
         if (m_hWnd != nullptr)
         {
             MessageLoop();
